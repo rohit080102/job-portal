b080bc38d87b76f393aa579dabcc31abad85d5d5
pkmodi2022 <payalmodi2017@gmail.com>
2023-05-01 17:56:24 +0530

    registration page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './views/pages/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'auth/sign-in' },
+  {
+    path: 'auth/sign-in',
+    component: RegistrationComponent,
+    title: 'Registration',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
